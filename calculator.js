993e8277e43fd3d42c55e23578b4c815c6eb0dd5
Burlry <burlry@mail.gvsu.edu>
2024-09-28 19:36:59 -0400

    create initial js html and css pages

---
+++ calculator.js
@@ -0,0 +1,50 @@
+const container = document.querySelector('#container')
+const display = document.createElement('div')
+const displayText = document.createElement('p')
+const rowContainer = document.createElement('div')
+
+container.appendChild(rowContainer)
+container.appendChild(rowContainer)
+container.appendChild(rowContainer)
+container.appendChild(rowContainer)
+container.appendChild(rowContainer)
+
+
+
+
+
+
+
+const add = function(a,b) {
+    return a + b;
+};
+
+const subtract = function(a,b) {
+    return a - b;
+};
+
+const multiply = function(a,b) {
+    return a * b;
+}
+
+const divide = function(a,b) {
+    return a / b;
+}
+
+let num1;
+let num2;
+let operand;
+
+const operate = function(a,b,operator) {
+    if (operator === '+') {
+        return add(a,b)
+    } else if (operator === '-') {
+        return subtract(a,b)
+    } else if (operator === '*') {
+        return multiply(a,b)
+    } else if (operator === '/') {
+        return divide(a,b)
+    } else {
+        return "ERROR";
+    };
+};
