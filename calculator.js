c3229898f0fe69dcf79eac3be3db5f472e8c8723
Burlry <burlry@mail.gvsu.edu>
2024-10-01 09:57:09 -0400

    add beginning of basic functions

--- calculator.js
+++ calculator.js
@@ -1,12 +1,5 @@
-const container = document.querySelector('#container')
-const display = document.createElement('div')
-const displayText = document.createElement('p')
-
-
-
-
-
-
+const container = document.querySelector('#container');
+const display = document.querySelector('#display');
 
 const add = function(a,b) {
     return a + b;
@@ -18,10 +11,14 @@ const subtract = function(a,b) {
 
 const multiply = function(a,b) {
     return a * b;
-}
+};
 
 const divide = function(a,b) {
     return a / b;
+};
+
+const remainder = function(a,b) {
+    return a % b;
 }
 
 let num1;
@@ -37,7 +34,39 @@ const operate = function(a,b,operator) {
         return multiply(a,b)
     } else if (operator === '/') {
         return divide(a,b)
+    } else if (operator === '%') {
+        return remainder(a,b)
     } else {
         return "ERROR";
     };
 };
+
+let buttons = document.querySelectorAll(".button");
+buttons.forEach((button) => {
+    button.onclick = () => {
+        if (button.textContent == 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9) {
+            numberPress()
+        } else if (button.textContent == "%" || "/" || "*" || "-" || "+") {
+            operandPress()
+        }
+    };
+});
+
+const numberPress = function() {
+    if (operandLast = false) {
+        if (display.textContent === 0) {
+            button.textContent = display.textContent;
+        } else {
+            button.textContent = display.textContent + button.textContent
+        }
+    } else {
+
+    }
+};
+
+const operandPress = function() {
+    operand = button.textContent
+    operandLast = true;
+}
+
+let operandLast = false;
